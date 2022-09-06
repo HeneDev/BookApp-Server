b65f10f936f8f656064ed581b5da1689188e288f
HeneDev <44865736+HeneDev@users.noreply.github.com>
2022-09-06 12:36:46 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -27,5 +27,5 @@ app.use("/api/books", bookRoute)
 
 // Start the server
 app.listen(process.env.PORT, () => {
-    console.log("Server running on port", 3001)
+    console.log("Server running on port", process.env.PORT)
 })
